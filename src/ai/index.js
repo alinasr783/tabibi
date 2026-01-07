4e809e776f7b5879c7145ca0a190724f704aa499
alinasr783 <alinasreldin783@gmail.com>
2026-01-07 23:17:15 +0200

    Update index.js

--- src/ai/index.js
+++ src/ai/index.js
@@ -5,10 +5,9 @@
 export { default as AskTabibiPage } from './ui/AskTabibiPage';
 export { default as ActionRenderer } from './ui/ActionRenderer';
 export { default as useChat } from './ui/useChat';
-export { default as PhaseIndicator, CompactPhaseIndicator, PhaseTimeline } from './ui/PhaseIndicator';
 
 // Services
-export * from '../services/apiAskTabibi';
+export * from './services/apiAskTabibi';
 
 // Core AI functionality
 export * from './services/aiActions';
@@ -16,6 +15,3 @@ export * from './services/aiContext';
 export * from './services/aiSystemPrompt';
 export * from './services/aiUtils';
 export * from './services/aiCore';
-
-// Pipeline System
-export * from './services/aiPipeline';
