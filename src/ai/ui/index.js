14eeb25d265312657f35c99c195ede9172c5868a
alinasr783 <alinasreldin783@gmail.com>
2026-01-07 23:16:22 +0200

    Update index.js

--- src/ai/ui/index.js
+++ src/ai/ui/index.js
@@ -1,6 +1,3 @@
 export { default as AskTabibiPage } from "./AskTabibiPage";
 export { useChat, useConversations, useMessages, useSendMessage } from "./useChat";
 export { ActionRenderer, parseAIResponse, FormattedText, InlineIcon, InlineMessageRenderer, ACTION_TYPES } from "./ActionRenderer";
-export { default as PhaseIndicator, CompactPhaseIndicator, PhaseTimeline } from "./PhaseIndicator";
-export { default as ChartRenderer, LineChartRenderer, BarChartRenderer, PieChartRenderer } from "./ChartRenderer";
-export { default as TableRenderer, ComparisonTableRenderer } from "./TableRenderer";
